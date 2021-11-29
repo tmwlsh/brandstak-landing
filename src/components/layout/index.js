faa03ce6fc28decc4abfe3606765360de4bbca69
Tom Walsh <hello@tmwlsh.co.uk>
2021-11-29 21:27:22 +0000

    feat: sorts fonts

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -4,6 +4,7 @@ import Footer from "../footer";
 import Socials from "../socials";
 
 import "../../sass/basics.scss";
+import "../../sass/typography.scss";
 
 import * as styles from "./styles.module.scss";
 
