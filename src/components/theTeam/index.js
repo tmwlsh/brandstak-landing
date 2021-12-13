57917da156459ca286e520188285ec302e301c87
TMWLSH <hello@tmwlsh.co.uk>
2021-12-13 21:32:52 +0000

    feat: adds staff images

--- src/components/theTeam/index.js
+++ src/components/theTeam/index.js
@@ -3,6 +3,11 @@ import Container from "../container";
 
 import * as styles from "./styles.module.scss";
 
+import Staff1 from "../../images/staff-1.jpg";
+import Staff2 from "../../images/staff-2.jpg";
+import Staff3 from "../../images/staff-3.jpg";
+import Staff4 from "../../images/staff-4.jpg";
+
 const TheTeam = () => {
   return (
     <div className={styles.theTeam} id="the-team">
@@ -11,19 +16,19 @@ const TheTeam = () => {
         <div className={styles.theTeamInner}>
           <ul className={styles.teamList}>
             <li>
-              <div className={styles.teamImage} />
+              <img className={styles.teamImage} src={Staff1} alt="Staff Member Team Photo" />
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in ornare augue, at maximus magna. Nunc consequat lorem a pretium bibendum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel sem vel ante gravida hendrerit vulputate non odio. Fusce in nunc mollis, placerat velit ut, pellentesque felis.</p>
             </li>
             <li>
-              <div className={styles.teamImage} />
+              <img className={styles.teamImage} src={Staff2} alt="Staff Member Team Photo" />
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in ornare augue, at maximus magna. Nunc consequat lorem a pretium bibendum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel sem vel ante gravida hendrerit vulputate non odio. Fusce in nunc mollis, placerat velit ut, pellentesque felis.</p>
             </li>
             <li>
-              <div className={styles.teamImage} />
+              <img className={styles.teamImage} src={Staff3} alt="Staff Member Team Photo" />
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in ornare augue, at maximus magna. Nunc consequat lorem a pretium bibendum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel sem vel ante gravida hendrerit vulputate non odio. Fusce in nunc mollis, placerat velit ut, pellentesque felis.</p>
             </li>
             <li>
-              <div className={styles.teamImage} />
+              <img className={styles.teamImage} src={Staff4} alt="Staff Member Team Photo" />
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in ornare augue, at maximus magna. Nunc consequat lorem a pretium bibendum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel sem vel ante gravida hendrerit vulputate non odio. Fusce in nunc mollis, placerat velit ut, pellentesque felis.</p>
             </li>
           </ul>
